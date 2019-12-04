d600390340f880dd060086bd60da9ab9af08c779
babazze <babazze@live.fr>
2019-12-04 10:09:17 +0100

    fini

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="fr" >
+  <head>
+    <meta charset="utf-8" />
+    <title>part4ex2</title>
+  </head>
+  <body>
+    <?php
+    function uno($name = 'Azz' , $firstname = 'Att' , $age = '26' ) {
+        return ('Bonjour ' . $name . ' ' . $firstname . ' tu as ' . $age . 'ans.' );
+    }
+      echo uno ()
+    ?>
+  </body>
+</html>
